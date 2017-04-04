f0e074c07c542692fdd740232d5252d5a8c36863
jho36 <jho36@Jho-PC>
2017-04-04 14:10:10 +0900

    static int[] remove(int[] s1, int[] s2) 메소드 추가

--- 실습 04-04/src/ArrayUtility2.java
+++ 실습 04-04/src/ArrayUtility2.java
@@ -18,25 +18,38 @@ public class ArrayUtility2
 {
     static int[] concat(int[] s1, int[] s2)
     {
-        int[] iTempAry = new int[s1.length + s2.length];
+        int[] iTempAry1 = new int[s1.length + s2.length];
 
         for (int i = 0; i < s1.length; i++)
         {
-            iTempAry[i] = s1[i];
+            iTempAry1[i] = s1[i];
         }
 
         for (int i = 0; i < s2.length; i++)
         {
-            iTempAry[i + s1.length] = s2[i];
+            iTempAry1[i + s1.length] = s2[i];
         }
 
-        return iTempAry;
+        return iTempAry1;
 
     }
-    
-    static int [] remove(int [] s1, int [] s2)
+
+    static int[] remove(int[] s1, int[] s2)
     {
-        
+        int[] iTempAry2 = new int[s1.length + s2.length];
+        int num = 0;
+        for (int i = 0; i < s1.length; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < s2.length; j++)
+            {
+                if (s1[i] == s2[j]) count += 1;
+            }
+            if (count == 0) iTempAry2[num++] = s1[i];
+
+        }
+
+        return iTempAry2;
     }
 
     public static void main(String args[])
@@ -52,12 +65,12 @@ public class ArrayUtility2
         System.out.print("Enter three numbers >> ");
         for (int i = 0; i < 3; i++)
             iAry2[i] = input.nextInt();
-        
+
         int length = iAry1.length + iAry2.length;
-        int [] tempAry = concat(iAry1, iAry2);
-        
-       for(int num : tempAry)
-           System.out.println(num);
-        
+        int[] tempAry = concat(iAry1, iAry2);
+
+        for (int num : tempAry)
+            System.out.println(num);
+
     }
 }
