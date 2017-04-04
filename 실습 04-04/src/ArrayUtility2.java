d690678c2443945a51b35ae292e6f74b71d8f2f9
jho36 <jho36@Jho-PC>
2017-04-04 13:51:33 +0900

    BackUp

---
+++ 실습 04-04/src/ArrayUtility2.java
@@ -0,0 +1,63 @@
+/**
+ * @date : 2017. 4. 4.
+ */
+/**
+ * <pre>
+ *  
+ *    |_ ArrayUtility2.java
+ * </pre>
+ * 
+ * @date : 2017. 4. 4. ¿ÀÀü 12:57:06
+ * @version :
+ * @author : Jho
+ */
+
+import java.util.Scanner;
+
+public class ArrayUtility2
+{
+    static int[] concat(int[] s1, int[] s2)
+    {
+        int[] iTempAry = new int[s1.length + s2.length];
+
+        for (int i = 0; i < s1.length; i++)
+        {
+            iTempAry[i] = s1[i];
+        }
+
+        for (int i = 0; i < s2.length; i++)
+        {
+            iTempAry[i + s1.length] = s2[i];
+        }
+
+        return iTempAry;
+
+    }
+    
+    static int [] remove(int [] s1, int [] s2)
+    {
+        
+    }
+
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+
+        int[] iAry1 = new int[2];
+        System.out.print("Enter two numbers >> ");
+        for (int i = 0; i < 2; i++)
+            iAry1[i] = input.nextInt();
+
+        int[] iAry2 = new int[3];
+        System.out.print("Enter three numbers >> ");
+        for (int i = 0; i < 3; i++)
+            iAry2[i] = input.nextInt();
+        
+        int length = iAry1.length + iAry2.length;
+        int [] tempAry = concat(iAry1, iAry2);
+        
+       for(int num : tempAry)
+           System.out.println(num);
+        
+    }
+}
